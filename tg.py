c0e7f8bc58e5ff1dd9ed0c13b6dd720b24401fb0
robot4black <myrobotblack@outlook.com>
2025-08-04 01:16:34 +0800

    Update tg.py

--- tg.py
+++ tg.py
@@ -4,12 +4,13 @@ import requests
 TG_BOT_TOKEN = os.getenv('TG_BOT_TOKEN')
 TG_CHAT_ID = os.getenv('TG_CHAT_ID')
 
+url = f'https://api.telegram.org/bot{TG_BOT_TOKEN}/sendMessage'
 
 def send_file_content(filename):
     with open(filename, 'r') as f:
         content = f.read()
     data = {
-        'chat_id': chat_id,
+        'chat_id': TG_CHAT_ID,
         'text': content
     }
     resp = requests.post(url, data=data)
